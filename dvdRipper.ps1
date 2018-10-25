709680ad295ef2ab2e6f56780dd17df553c0f2af
Ray Sanders <raymond.sanders@gmail.com>
2018-10-24 23:13:58 -0500

    automate adding spaces to movie name

--- dvdRipper.ps1
+++ dvdRipper.ps1
@@ -5,4 +5,14 @@ Param(
 
 .\..\HandBrakeCLI-1.1.2-win-x86_64\HandBrakeCLI.exe -i D:\ --main-feature -o C:\MovieRips\"$movieName($year)".m4v
 
+Write-Output "Moving file to server"
 Move-Item -Path "C:\MovieRips\$movieName($year).m4v" -Destination \\rvsserv1\video\Movie -Force
+Write-Output "Move to server complete!"
+
+Write-Output "Renaming File"
+
+$formattedMovieName = ($movieName -creplace '[^a-z\s]', ' $&').Trim()
+
+Rename-Item -Path "\\rvsserv1\video\Movie\$movieName`($year).m4v" -NewName "\\rvsserv1\video\Movie\$formattedMovieName` ($year).m4v"
+
+Write-Output "Script complete!!"
