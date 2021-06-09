a861429c17bdb50ef21b3e4110fe5b821d90347b
Ray Sanders <raymond.sanders@gmail.com>
2021-06-09 17:40:56 -0500

    Adding parameter for format

--- dvdRipper.ps1
+++ dvdRipper.ps1
@@ -1,18 +1,27 @@
 Param(
-    [string]$movieName,
-    [string]$year
+    # [string]$movieName,
+    # [string]$year
+    [Parameter(Mandatory)]
+    [ValidateSet("movie", "tv", IgnoreCase=$true)]
+    [string]$format
 )
 
-.\..\HandBrakeCLI-1.1.2-win-x86_64\HandBrakeCLI.exe -i D:\ --main-feature -o C:\MovieRips\"$movieName($year)".m4v
+if($format -eq "movie") {
+    Write-Output "Hi movie"
+} else {
+    Write-Output "Hi tv show"   
+}
 
-Write-Output "Moving file to server"
-Move-Item -Path "C:\MovieRips\$movieName($year).m4v" -Destination \\rvsserv1\video\Movie -Force
-Write-Output "Move to server complete!"
+# .\..\HandBrakeCLI-1.1.2-win-x86_64\HandBrakeCLI.exe -i D:\ --main-feature -o C:\MovieRips\"$movieName($year)".m4v
 
-Write-Output "Renaming File"
+# Write-Output "Moving file to server"
+# Move-Item -Path "C:\MovieRips\$movieName($year).m4v" -Destination \\rvsserv1\video\Movie -Force
+# Write-Output "Move to server complete!"
 
-$formattedMovieName = ($movieName -creplace '[^a-z\s]', ' $&').Trim()
+# Write-Output "Renaming File"
 
-Rename-Item -Path "\\rvsserv1\video\Movie\$movieName`($year).m4v" -NewName "\\rvsserv1\video\Movie\$formattedMovieName` ($year).m4v"
+# $formattedMovieName = ($movieName -creplace '[^a-z\s]', ' $&').Trim()
+
+# Rename-Item -Path "\\rvsserv1\video\Movie\$movieName`($year).m4v" -NewName "\\rvsserv1\video\Movie\$formattedMovieName` ($year).m4v"
 
 Write-Output "Script complete!!"
