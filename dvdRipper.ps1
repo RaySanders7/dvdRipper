b741054858b0c33857fd8f4c532a168819ea45e2
Ray Sanders <raymond.sanders@gmail.com>
2021-06-09 18:00:05 -0500

    Add logic for setting the movie name and release year

--- dvdRipper.ps1
+++ dvdRipper.ps1
@@ -8,20 +8,27 @@ Param(
 
 if($format -eq "movie") {
     Write-Output "Hi movie"
+
+    $movieName = Read-Host -Prompt 'Movie Name? (With No Spaces in the Name)'
+    $year = Read-Host -Prompt 'Year released?'
+
+    Write-Output "Preparing to rip movie $movieName($year)..." 
+
+    .\..\HandBrakeCLI-1.1.2-win-x86_64\HandBrakeCLI.exe -i D:\ --main-feature -o C:\MovieRips\"$movieName($year)".m4v
+
+    Write-Output "Moving movie to server"
+    Move-Item -Path "C:\MovieRips\$movieName($year).m4v" -Destination \\rvsserv1\video\Movie -Force
+    Write-Output "Move to server complete!"
+
+    Write-Output "Renaming Movie"
+
+    $formattedMovieName = ($movieName -creplace '[^a-z\s]', ' $&').Trim()
+
+    Rename-Item -Path "\\rvsserv1\video\Movie\$movieName`($year).m4v" -NewName "\\rvsserv1\video\Movie\$formattedMovieName` ($year).m4v"
 } else {
     Write-Output "Hi tv show"   
 }
 
-# .\..\HandBrakeCLI-1.1.2-win-x86_64\HandBrakeCLI.exe -i D:\ --main-feature -o C:\MovieRips\"$movieName($year)".m4v
 
-# Write-Output "Moving file to server"
-# Move-Item -Path "C:\MovieRips\$movieName($year).m4v" -Destination \\rvsserv1\video\Movie -Force
-# Write-Output "Move to server complete!"
-
-# Write-Output "Renaming File"
-
-# $formattedMovieName = ($movieName -creplace '[^a-z\s]', ' $&').Trim()
-
-# Rename-Item -Path "\\rvsserv1\video\Movie\$movieName`($year).m4v" -NewName "\\rvsserv1\video\Movie\$formattedMovieName` ($year).m4v"
 
 Write-Output "Script complete!!"
