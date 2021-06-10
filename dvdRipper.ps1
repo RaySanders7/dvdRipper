98e2251f2babf493c287fcdd3d825d3b15fcbeb7
Ray Sanders <raymond.sanders@gmail.com>
2021-06-10 08:24:24 -0500

    Add logic for ripping a tv show based on inputs for episode range

--- dvdRipper.ps1
+++ dvdRipper.ps1
@@ -1,13 +1,11 @@
 Param(
-    # [string]$movieName,
-    # [string]$year
     [Parameter(Mandatory)]
     [ValidateSet("movie", "tv", IgnoreCase=$true)]
     [string]$format
 )
 
 if($format -eq "movie") {
-    Write-Output "Hi movie"
+    Write-Output "Ripping a movie!"
 
     $movieName = Read-Host -Prompt 'Movie Name? (With No Spaces in the Name)'
     $year = Read-Host -Prompt 'Year released?'
@@ -26,7 +24,28 @@ if($format -eq "movie") {
 
     Rename-Item -Path "\\rvsserv1\video\Movie\$movieName`($year).m4v" -NewName "\\rvsserv1\video\Movie\$formattedMovieName` ($year).m4v"
 } else {
-    Write-Output "Hi tv show"   
+    Write-Output "Ripping a TV show!"
+
+    $showName = Read-Host -Prompt 'TV Show Name? (With No Spaces in the Name)'
+    $seasonNumber = Read-Host -Prompt 'Season number (With Format XX)?'
+    $stringEpisodeNumberStart = Read-Host -Prompt "Starting episode number? (With Format XX)"
+    $stringEpisodeNumberEnd = Read-Host -Prompt "Ending episode number? (With Format XX)"
+
+    $intEpisodeNumberStart = [int]$stringEpisodeNumberStart
+    $intEpisodeNumberEnd = [int]$stringEpisodeNumberEnd
+
+    $episodeCount = $intEpisodeNumberEnd - $intEpisodeNumberStart + 1
+
+    Write-Output "Preparing to rip TV show $showName Season $seasonNumber..." 
+    Write-Output "Start = $intEpisodeNumberStart" 
+    Write-Output "End = $intEpisodeNumberEnd" 
+    Write-Output "End = $episodeCount" 
+
+    for ($i = 0; $i -lt $episodeCount; $i++) {
+        $currentTitleNumber = $i + 1
+        Write-Output "Ripping episode $currentTitleNumber ..." 
+        # .\..\HandBrakeCLI-1.1.2-win-x86_64\HandBrakeCLI.exe -i D:\ --title $currentTitleNumber -o C:\MovieRips\"$showName"\"Season$seasonNumber\$showName-s"
+    }
 }
 
 
